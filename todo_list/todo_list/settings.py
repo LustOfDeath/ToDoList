f4ddb5b5ff279536d62614467d7c5d8e7913fedd
Paulius <pirkanetas@gmail.com>
2023-02-10 11:05:44 +0200

    security warning

--- todo_list/todo_list/settings.py
+++ todo_list/todo_list/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!e_#+8a_ms8nf(%uhqv28ov%x3nf2*el-qyj+buxawv=bq*_1='
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
